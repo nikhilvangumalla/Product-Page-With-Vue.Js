a3952fc2c1488e5511aa8cb737d351482b00995d
Nikhil Vangumalla <nikhilvangumalla@gmail.com>
2020-03-14 18:19:42 +0530

    Feature list rendering

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ let app = new Vue({
   data: {
     productName: 'Amer Lorn Chair',
     image: './images/Amer Lorn Chair.webp',
-    inStock: false
+    inStock: true,
+    details: [
+      '100% Mango Wood',
+      'Assembly - No',
+      'Size - 62cm x 72cm x 82cm',
+      'Weight - 12.1 Kg'
+    ],
+    varients: [
+      {
+        varientId: 10627504,
+        varinetColor: 'Dark Grey'
+      },
+      {
+        varientId: 10627505,
+        varinetColor: 'Light Grey'
+      }
+    ]
   }
 });
